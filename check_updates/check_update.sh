e9ba7afc7a26773768746accd895d5394f1fb8ac
Yosuke Matsumura <52749251+GuiltyDoggy@users.noreply.github.com>
2022-12-02 10:26:51 -0600

    change script to use skopeo only, instead of digest.txt to detect upstream changes. But this script will build locally, not using quay.io build system.

--- check_updates/check_update.sh
+++ check_updates/check_update.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 # This script will be run from inside the Check Update container
 
-# Get latest digest.txt from GitHub in case this has been updated elsewhere
-git pull --no-rebase
 
-# Get digest of latest image from the official base image
-export DIGEST=$(skopeo inspect  --format "Digest: {{.Digest}}" docker://quay.io/fedora-ostree-desktops/kinoite:37)
+# Get the ostree.commit of the official base image of Kinoite
+export UPSTREAM_DIGEST=$(skopeo inspect -n docker://quay.io/fedora-ostree-desktops/kinoite:37 | jq -r '.Labels["ostree.commit"]')
 
-# Get digest of last image from the digest.txt file 
-export LAST_DIGEST=$(cat digest.txt)
+# Get the ostree.commit of our latest custom image 
+export OUR_DIGEST=$(skopeo inspect -n docker://https://quay.io/repository/guiltydoggy/kinoite-gd:latest | jq -r '.Labels["ostree.commit"]')
 
 # Check if the digests are the same
-if [ "$DIGEST" = "$LAST_DIGEST" ]; then
+if [ "$UPSTREAM_DIGEST" = "$LAST_DIGEST" ]; then
     echo "Image has not changed"
 else
-    # If the digest of the base image is new, update the digest.txt file and push it to the git repository, triggering the new build at quay.io
-    echo "New image is available"
-    echo $DIGEST > digest.txt
-    git add digest.txt
-    git commit -m "Digest changed. $DIGEST"
-    git push 
+    # New base image is available, start new custom image build
+    echo "New image available. Starting build"
+    set IMAGE (podman build -q -t kinoite-gd -f Containerfile)
+    podman login -u guiltydoggy -p $QUAY_PASS quay.io/guiltydoggy 
+    podman push $IMAGE docker.io/guiltydoggy/kinoite-gd:latest
 fi
 
-echo $LAST_DIGEST
-echo $DIGEST
+echo "Old: $LAST_DIGEST"
+echo "New: $UPSTREAM_DIGEST"
